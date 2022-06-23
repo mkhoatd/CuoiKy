692dd34f3d486fe9e4398165724e0072caffef08
mkhoatd <binnhatle@gmail.com>
2022-06-23 09:13:33 +0700

    add sinhvienbll

---
+++ CuoiKy/BLL/SinhVienBLL.cs
@@ -0,0 +1,69 @@
+﻿using CuoiKy.DAL;
+using CuoiKy.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuoiKy.BLL
+{
+    public class SinhVienBLL
+    {
+        private readonly DiemHocPhanDbContext _context;
+
+        private SinhVienBLL()
+        {
+            _context = new DiemHocPhanDbContext();
+        }
+        private SinhVienBLL _instance { get; set; }
+        public SinhVienBLL Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new SinhVienBLL();
+                return _instance;
+            }
+            private set { }
+        }
+        public List<SinhVienDTO> GetAllSinhVienDTOs(string name = "", string TenHocPhan = "", string orderBy = "")
+        {
+            var result = _context.SinhViens.Select(s => new SinhVienDTO(s))
+                .Where(s => String.IsNullOrEmpty(name) ? true : s.TenSinhVien.Contains(name))
+                .Where(s => string.IsNullOrEmpty(TenHocPhan) ? true : s.TenHocPhan.Contains(TenHocPhan));
+            return result.ToList();
+        }
+        public void AddOrUpdate(SinhVienDTO sv)
+        {
+            if (_context.SinhViens.Any(s => s.Id == sv.Id))
+            {
+                var s = _context.SinhViens.Find(sv.Id);
+                s.TenSinhVien = sv.TenSinhVien;
+                s.LopSinhHoat = sv.LopSinhHoat;
+                s.GioiTinh = sv.GioiTinh;
+                s.DiemBaiTap = sv.DiemBaiTap;
+                s.DiemGiuaKy = sv.DiemGiuaKy;
+                s.NgayThi = sv.NgayThi;
+                s.HocPhanId = _context.HocPhans.FirstOrDefault(hp => hp.TenHocPhan == sv.TenHocPhan).Id;
+            }
+            else
+            {
+                _context.SinhViens.Add(new DAO.SinhVien
+                {
+                    Id = System.Guid.NewGuid().ToString(),
+                    TenSinhVien = sv.TenSinhVien,
+                    LopSinhHoat = sv.LopSinhHoat,
+                    GioiTinh = sv.GioiTinh,
+                    DiemBaiTap = sv.DiemBaiTap,
+                    DiemGiuaKy = sv.DiemGiuaKy,
+                    NgayThi = sv.NgayThi,
+                    HocPhanId = _context.HocPhans.FirstOrDefault(hp => hp.TenHocPhan == sv.TenHocPhan).Id
+                });
+            }
+        }
+        public void Delete(List<string> SinhvienIds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
